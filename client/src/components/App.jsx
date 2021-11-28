b753838aa4a8267462f46f1de8fff1691f1e7747
ag-shubham <shubhamsatnalika64@gmail.com>
2021-11-28 14:35:30 +0530

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+// import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+    return (
+        <div>
+            <Home />
+
+        </div>
+    );
+}
+
+export default App;
