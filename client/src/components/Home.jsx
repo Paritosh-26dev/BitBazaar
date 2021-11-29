45400d4ecede14b179ce3275bff6f049e82190b3
lokicodes <lokeshsharma.20020219@gmail.com>
2021-11-29 14:27:38 +0530

    home page, contact page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Login from "./Login";
+import "../../src/style.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function Home(){
-    return(
-        <Navbar />
-    );
+function Home() {
+  return (
+    <div >
+      <div class="bg-image"></div>
+        <div class="bg-text">
+          <Navbar />
+          <div class="row" >
+          <div class="col-sm-6">
+            <Login />
+          </div>
+          <div class="col-sm-6 bitbazaar-intro" >
+            <h1 >
+              Welcome To <span> BIT Bazaar </span>. Your All in One Platform to Buy , Sell And Trade At College. 
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home ;
+export default Home;
