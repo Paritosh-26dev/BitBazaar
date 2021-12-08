8a1afc1e51e7062101bc240720b151055f3cffa2
lokicodes <lokeshsharma.20020219@gmail.com>
2021-12-08 17:59:29 +0530

    saving sell form data to atlas

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 //connecting to mongodb atlas->BitBazaar database
 mongoose.connect("mongodb+srv://bitbazaar:klpd@cluster0.hh4t5.mongodb.net/testDB");
 
+//below code is contributed by team lobham xd
 app.use("/" , require("./routes/itemRoute")) ;
 
 const PORT = process.env.PORT || 5000;
